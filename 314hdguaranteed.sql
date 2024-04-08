9ba82b692ec7c86f0dbaa8cd7da62fd455edd49c
affcue <165614848+affcue@users.noreply.github.com>
2024-04-08 13:51:43 +0800

    Create 314hdguaranteed.sql

---
+++ 314hdguaranteed.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3307
+-- Generation Time: Apr 08, 2024 at 07:50 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `314hdguaranteed`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `listing`
+--
+
+CREATE TABLE `listing` (
+  `listing_id` int(11) NOT NULL,
+  `ra_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `region` enum('North','South','East','West','Central') NOT NULL,
+  `type` enum('Condominium','Landed Property','HDB Flat') NOT NULL,
+  `location` text NOT NULL,
+  `postal` int(6) NOT NULL,
+  `price` int(11) NOT NULL,
+  `rooms` int(1) NOT NULL,
+  `size` int(11) NOT NULL,
+  `views` int(11) DEFAULT NULL,
+  `shortlists` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ra`
+--
+
+CREATE TABLE `ra` (
+  `ra_id` int(11) NOT NULL,
+  `e-mail` varchar(150) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `contact` int(8) NOT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `rating_id` int(11) NOT NULL,
+  `ra_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `stars` int(11) NOT NULL,
+  `descrition` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shortlist`
+--
+
+CREATE TABLE `shortlist` (
+  `listing_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `e-mail` varchar(150) NOT NULL,
+  `username` int(50) NOT NULL,
+  `password` int(50) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `contact` int(11) NOT NULL,
+  `purpose` enum('buyer','seller') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `listing`
+--
+ALTER TABLE `listing`
+  ADD PRIMARY KEY (`listing_id`),
+  ADD KEY `ra_id` (`ra_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `ra`
+--
+ALTER TABLE `ra`
+  ADD PRIMARY KEY (`ra_id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`rating_id`),
+  ADD KEY `ra_id` (`ra_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `shortlist`
+--
+ALTER TABLE `shortlist`
+  ADD KEY `listing_id` (`listing_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `e-mail` (`e-mail`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `listing`
+--
+ALTER TABLE `listing`
+  MODIFY `listing_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ra`
+--
+ALTER TABLE `ra`
+  MODIFY `ra_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
