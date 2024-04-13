1374dab98584a7bb559e6dc3ce017081e1279255
affcue <165614848+affcue@users.noreply.github.com>
2024-04-13 12:21:48 +0800

    Update db_connection.php

--- Control/db_connection.php
+++ Control/db_connection.php
@@ -12,12 +12,12 @@ $dbname = "314hdguaranteed";
 */
 
 //FQs db login
-
+/*
 $servername = "127.0.0.1:3307";
 $username = "root";
 $password = ""; 
 $dbname = "314hdguaranteed";
-
+*/
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
