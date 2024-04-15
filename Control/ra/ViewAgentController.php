42a32fc425ae04ae640400385330f34184bc646d
joncool99 <joncool99@hotmail.com>
2024-04-16 01:13:35 +0800

    Create ViewAgentController.php
    added view agent controller

---
+++ Control/ra/ViewAgentController.php
@@ -0,0 +1,21 @@
+<?php
+require_once("../entity/ra/ViewAgentEntity.php");
+
+class ViewAgentController
+{
+    private $entity;
+
+    public function __construct()
+    {
+        $this->entity = new ViewAgentEntity();
+    }
+
+    public function displayAgentInfo($agentName)
+    {
+        // Retrieve agent information based on the provided name
+        $agent = $this->entity->fetchAgentByName($agentName);
+
+        // Return the agent details
+        return $agent;
+    }
+}
