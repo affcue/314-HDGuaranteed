e1c17c0e6a917956736efc9b172e1c7ab6df3ce8
joncool99 <joncool99@hotmail.com>
2024-04-16 01:13:45 +0800

    Create db_connection.php
    added db connection for ra entity

---
+++ Entity/ra/db_connection.php
@@ -0,0 +1,36 @@
+<?php
+class DBConnection
+{
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname = "314hdguaranteed";
+    private $conn;
+
+    
+
+    // Constructor to initialize database connection
+    public function __construct()
+    {
+        // Create connection
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    // Method to get the database connection
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+
+    // Method to close the database connection
+    public function closeConnection()
+    {
+        $this->conn->close();
+    }
+}
+?>
