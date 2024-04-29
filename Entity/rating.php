a557c7fd1e5affbab3dff029ecfcd2c95270a181
jtwr171 <jordantanwr@gmail.com>
2024-04-29 16:56:52 +0800

    removed sellerentity class

--- Entity/rating.php
+++ Entity/rating.php
@@ -19,42 +19,5 @@ class Rating
         $stmt->execute();
         $stmt->close();
     }
-
-    private $sellerEntity;
-
-    public function validateSellerLogin($username, $password)
-    {
-        $this->sellerEntity = new SellerEntity($this->conn);
-        return $this->sellerEntity->validateLogin($username, $password);
-    }
-}
-
-class SellerEntity {
-    private $conn;
-    private $table_name = "user";
-
-    public function __construct($db) {
-        $this->conn = $db;
-    }
-
-    public function validateLogin($username, $password) {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE username = ? AND password = ?";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-
-        if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
-            if ($row['purpose'] == 'seller') {
-                return $row['user_id'];
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
 }
 ?>
